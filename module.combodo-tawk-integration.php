373f9e00b56bcf0824aa642d7c88cc962fb13b0a
Molkobain <lajarige.guillaume@free.fr>
2021-11-09 11:29:51 +0100

    :bookmark: Prepare v1.0.0

--- module.combodo-tawk-integration.php
+++ module.combodo-tawk-integration.php
@@ -20,7 +20,7 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-tawk-integration/0.2.0',
+	'combodo-tawk-integration/1.0.0',
 	array(
 		// Identification
 		//
